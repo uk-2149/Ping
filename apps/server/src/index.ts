96b526a3111d95770bc69ede21b4d63c22e72dfd
uk-2149 <utkal2149@gmail.com>
2025-07-25 22:49:46 +0530

    Initial commit

---
+++ apps/server/src/index.ts
@@ -0,0 +1,17 @@
+import http from "http";
+import SocketService from "./services/socket";
+
+async function init() {
+    const socketService = new SocketService();
+
+    const httpServer = http.createServer();
+    const PORT = process.env.PORT || 8000;
+
+    socketService.io.attach(httpServer);
+
+    httpServer.listen(PORT, () => {
+        console.log(`HTTP server started at PORT: ${PORT}`);
+    })
+}
+
+init();
